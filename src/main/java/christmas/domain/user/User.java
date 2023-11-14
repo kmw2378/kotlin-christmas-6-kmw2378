0e95808e2fdc82be8b371c24723a87e93c4c3479
kmw2378 <rlarla677@gmail.com>
2023-11-15 07:33:40 +0900

    feat(christmas): User 클래스 추가 배지, 예약 내역을 저장하는 도메인 클래스를 추가하였습니다.

---
+++ src/main/java/christmas/domain/user/User.java
@@ -0,0 +1,40 @@
+package christmas.domain.user;
+
+import christmas.domain.badge.Badge;
+import christmas.domain.benefit.BenefitType;
+import christmas.domain.menu.Menu;
+import christmas.domain.reservation.Reservation;
+
+public class User {
+    private final Badge badge;
+    private final Reservation reservation;
+
+    public User(final Badge badge, final Reservation reservation) {
+        this.badge = badge;
+        this.reservation = reservation;
+    }
+
+    public boolean equalsBadge(final Badge badge) {
+        return this.badge.equals(badge);
+    }
+
+    public boolean existBenefitType(final BenefitType benefitType) {
+        return reservation.existBenefitType(benefitType);
+    }
+
+    public boolean existMenu(final Menu menu) {
+        return reservation.existMenu(menu);
+    }
+
+    public long getTotalOrderAmount() {
+        return reservation.getTotalOrderAmount();
+    }
+
+    public long getTotalBenefitAmountFromType(final BenefitType benefitType) {
+        return reservation.getTotalBenefitAmountFromType(benefitType);
+    }
+
+    public int getQuantityFromMenu(final Menu menu) {
+        return reservation.getQuantityFromMenu(menu);
+    }
+}
