5c9407a796156554fb6d78e8a49c466942c43bdb
kmw2378 <rlarla677@gmail.com>
2023-11-15 01:42:35 +0900

    feat(christmas): InputView 인터페이스에 상수 및 추상 메서드 추가 주문 입력시 사용될 상수, 추상 메서드를 추가하였습니다.

--- src/main/java/christmas/view/InputView.java
+++ src/main/java/christmas/view/InputView.java
@@ -1,8 +1,17 @@
 package christmas.view;
 
+import christmas.dto.order.request.OrderRequest;
 import christmas.dto.reservaiton.request.VisitDateRequest;
+import java.util.List;
 
 public interface InputView {
     String VISIT_DATE_QUESTION_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    String ORDERS_QUESTION_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
+    String ORDER_INPUT_SEPARATOR = ",";
+    String MENU_NAME_QUANTITY_SEPARATOR = "-";
+    int MENU_NAME_INDEX = 0;
+    int MENU_QUANTITY_INDEX = 1;
+    int SEPARATED_ORDER_SIZE = 2;
     VisitDateRequest requestVisitDay();
+    List<OrderRequest> requestOrders();
 }
