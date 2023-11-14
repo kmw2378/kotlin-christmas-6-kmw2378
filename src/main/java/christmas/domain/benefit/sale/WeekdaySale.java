bd46f8955dbb0bbd26a0b6b674198d002bba3309
kmw2378 <rlarla677@gmail.com>
2023-11-15 01:59:14 +0900

    feat(christmas): WeekdaySale 구현 클래스 추가 평일 할인 혜택을 나타내는 클래스를 추가하였습니다.

---
+++ src/main/java/christmas/domain/benefit/sale/WeekdaySale.java
@@ -0,0 +1,30 @@
+package christmas.domain.benefit.sale;
+
+import christmas.domain.order.Orders;
+import christmas.domain.reservation.VisitDate;
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+
+import static christmas.config.PromotionConfig.WEEKDAY_PER_SALE_AMOUNT;
+import static christmas.domain.benefit.BenefitType.WEEKDAY;
+import static christmas.domain.menu.MenuType.DESERT;
+import static java.util.Calendar.SUNDAY;
+import static java.util.Calendar.THURSDAY;
+
+public class WeekdaySale extends Sale {
+    public WeekdaySale() {
+        super(WEEKDAY);
+    }
+
+    @Override
+    public boolean support(final LocalDate visitDate) {
+        final DayOfWeek convertedWeek = localDateToWeekConverter.convert(visitDate);
+        final int weekValue = convertedWeek.getValue();
+        return SUNDAY <= weekValue && weekValue <= THURSDAY;
+    }
+
+    @Override
+    public long getAmount(final Orders orders, final VisitDate visitDate) {
+        return orders.getTotalQuantityFromMenuType(DESERT) * WEEKDAY_PER_SALE_AMOUNT;
+    }
+}
