2f0ec97e79d6bb715b686e71b5bd8e5282d6090a
kmw2378 <rlarla677@gmail.com>
2023-11-15 13:32:44 +0900

    refactor(christmas): ErrorCode 필드 삭제 사용되지 않는 필드를 삭제했습니다.

--- src/main/java/christmas/exception/errorcode/ErrorCode.java
+++ src/main/java/christmas/exception/errorcode/ErrorCode.java
@@ -2,8 +2,7 @@ package christmas.exception.errorcode;
 
 public enum ErrorCode {
     INVALID_VISIT_DATE("유효하지 않은 날짜입니다."),
-    INVALID_ORDER("유효하지 않은 주문입니다."),
-    INVALID_GIFT_QUANTITY("수량이 잘못되었습니다.");
+    INVALID_ORDER("유효하지 않은 주문입니다.");
     private static final String PREFIX = "[ERROR] ";
     private static final String SUFFIX = " 다시 입력해 주세요.";
     private final String message;
